f8aae7bf4e4f4c28e68693527a42bdbc44d68991
arath <94558953+aramsesporcayomercado@users.noreply.github.com>
2025-03-29 17:55:40 -0700

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,6 +1,6 @@
 import os
 
-SECRET_KEY = os.getenv("SECRET_KEY",  "zerotwo")
+SECRET_KEY = os.getenv("SECRET_KEY",  "nutriMath")
 ALGORITHM = "HS256"
 
 
@@ -8,4 +8,4 @@ ALGORITHM = "HS256"
 MYSQL_HOST = os.getenv("MYSQL_HOST", "localhost")  # Host de la base de datos
 MYSQL_USER = os.getenv("MYSQL_USER", "root")       # Usuario de la base de datos
 MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "")    # Contraseña de la base de datos (vacía)
-MYSQL_DB = os.getenv("MYSQL_DB", "nutritec")          # Nombre de la base de datos
+MYSQL_DB = os.getenv("MYSQL_DB", "nutriMath")          # Nombre de la base de datos
