316a874b13b1c4213de718a0f99862f539ba4758
arath <94558953+aramsesporcayomercado@users.noreply.github.com>
2025-03-29 17:58:56 -0700

    Add files via upload

---
+++ nutriMath.sql
@@ -0,0 +1,121 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 30-03-2025 a las 01:54:16
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `nutriMath`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `consumo_energetico`
+--
+
+CREATE TABLE `consumo_energetico` (
+  `id` int(11) NOT NULL,
+  `matricula` int(11) DEFAULT NULL,
+  `mes` year(4) NOT NULL,
+  `consumo` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `planalimenticio`
+--
+
+CREATE TABLE `planalimenticio` (
+  `Codigo` int(11) NOT NULL,
+  `Proteina` float DEFAULT NULL,
+  `Carbohidrato` float DEFAULT NULL,
+  `Grasa` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `Matricula` int(11) NOT NULL,
+  `Nombre` varchar(50) DEFAULT NULL,
+  `ApellidoPaterno` varchar(50) DEFAULT NULL,
+  `ApellidoMaterno` varchar(50) DEFAULT NULL,
+  `Genero` varchar(10) DEFAULT NULL,
+  `PlanAlimenticio_codigo` int(11) DEFAULT NULL,
+  `Estatura` float DEFAULT NULL,
+  `Peso` float DEFAULT NULL,
+  `IMC` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `consumo_energetico`
+--
+ALTER TABLE `consumo_energetico`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `matricula` (`matricula`);
+
+--
+-- Indices de la tabla `planalimenticio`
+--
+ALTER TABLE `planalimenticio`
+  ADD PRIMARY KEY (`Codigo`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`Matricula`),
+  ADD KEY `PlanAlimenticio_codigo` (`PlanAlimenticio_codigo`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `consumo_energetico`
+--
+ALTER TABLE `consumo_energetico`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `consumo_energetico`
+--
+ALTER TABLE `consumo_energetico`
+  ADD CONSTRAINT `consumo_energetico_ibfk_1` FOREIGN KEY (`matricula`) REFERENCES `users` (`Matricula`);
+
+--
+-- Filtros para la tabla `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`PlanAlimenticio_codigo`) REFERENCES `planalimenticio` (`Codigo`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
